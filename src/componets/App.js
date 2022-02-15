1eb53b1ceedaf62eaf75a614c13427c0adf6f62f
Kevin Gallo <Gallok731@gmail.com>
2022-02-15 09:06:41 -0500

    sort and search

--- src/componets/App.js
+++ src/componets/App.js
@@ -6,7 +6,9 @@ import GamesContainer from "./GamesContainer";
 function App() {
 
   const [searchByName,setSearchByName] = useState('')
-  const [searchByNumPlayer,setSearchByNumPlayer] = useState(1)
+  
+  const [searchByNumPlayer,setSearchByNumPlayer] = useState(true)
+
   const [gameList,setGameList] = useState([])
 
 
@@ -17,22 +19,43 @@ function App() {
 
 
 
-  const changeSearchByNumPlayer = (e) => {
-    setSearchByNumPlayer(e.target.value)
+
+  const shownGames = [...gameList]
+
+
+
+  const changeSearchByNumPlayer = () => {
+    setSearchByNumPlayer(!searchByNumPlayer)
+    if(searchByNumPlayer){
+      shownGames.sort((a,b) => {
+        return a.playersMin - b.playersMin
+      })
+    }else{
+      shownGames.sort((a,b) => {
+        return a.id - b.id
+      })
+    }
+    setGameList(shownGames)
   }
 
 
 
-  const changeSearchBy = (e) => [
+  const changeSearchBy = (e) => {
     setSearchByName(e.target.value)
-  ]
+  }
+
+
+
+  const newArr = shownGames.filter(game => {
+    return game.name.toLowerCase().includes(searchByName.toLowerCase())
+  })
 
 
 
   return (
     <div>
       <Header changeSearchBy={changeSearchBy} changeSearchByNumPlayer={changeSearchByNumPlayer}/>
-      <GamesContainer gameList={gameList} />
+      <GamesContainer gameList={newArr} />
 
     </div>
   )
--- src/componets/Filter.js
+++ src/componets/Filter.js
@@ -5,17 +5,7 @@ function Filter({changeSearchBy,changeSearchByNumPlayer}) {
     <div>
         <p>
             <input onChange={changeSearchBy} type={'text'}/>
-            <select onChange={changeSearchByNumPlayer}>
-                <option>1-3 Player</option>
-                <option>4 Players</option>
-                <option>5+ Players</option>
-            </select>
-            {/* <select onChange={changeSearchByCat}>
-                <option>This one</option>
-                <option>This one</option>
-                <option>This one</option>
-                <option>This one</option>
-            </select> */}
+            <button onClick={changeSearchByNumPlayer}>Sort By Minimum Number Of Players</button>
         </p>
     </div>
   )
